6e07ce6583de736d40b1a26124e0bba298e77c48
Aadhie <ka.adithyakrishna2001@gmail.com>
2026-01-20 18:07:58 +0530

    adding terraform file

--- Start With Terraform Basics/main.tf
+++ Start With Terraform Basics/main.tf
@@ -2,7 +2,8 @@
 
 # Configure the AWS Provider
 provider "aws" {
-    version = "3.53.0"
+    # version = "3.53.0"
+    version = "6.28.0"
     access_key = "AKIAXC3FBB2OJJC3DUIP"
     secret_key = "SECRET_KEY"
     region     = "us-east-2"
@@ -10,6 +11,7 @@ provider "aws" {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-    version = "2.72.0"
+    # version = "2.72.0"
+    version = "4.57.0"
     features {}
 }
