d6dd31b49b0f9382f70c9440e2cb97f37d3da698
Aadhie <ka.adithyakrishna2001@gmail.com>
2026-01-20 18:51:13 +0530

    added profile to provider.tf

--- Start With Terraform Basics/provider.tf
+++ Start With Terraform Basics/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   # access_key = "AKIASMSIZOF4QD2NTPB6"
   # secret_key = "SECRET_KEY_HERE"
-  profile = "instructor-project"
+  profile = "terraform-proj"
   region     = "us-east-1"
 }
