702041da347952dfb624704e110072e1da4940c1
Aadhie <ka.adithyakrishna2001@gmail.com>
2026-01-21 15:20:30 +0530

    modified the instance type

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "MyFirstInstnace" {
   count         = 1
   ami           = "ami-0b0ea68c435eb488d"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "demoinstnce-${count.index}"
