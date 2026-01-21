93da1426d0e23cd8e6efe24000d2f4ee19be3538
Aadhie <ka.adithyakrishna2001@gmail.com>
2026-01-21 13:06:25 +0530

    added ami for us-east-1

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,8 +1,8 @@
 
 resource "aws_instance" "MyFirstInstnace" {
   count         = 3
-  ami           = "ami-0bff25b43a4479334"
-  instance_type = "t4g.micro"
+  ami           = "ami-0b0ea68c435eb488d"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "demoinstnce-${count.index}"
